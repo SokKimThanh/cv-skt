98aa5deee35ef0d38e7608240f0b09351c176cf9
sokkimthanh1 <thanhsk1991@gmail.com>
2021-02-10 14:53:39 +0700

    update work experience object

--- src/app/shared/workexperiences.ts
+++ src/app/shared/workexperiences.ts
@@ -18,7 +18,7 @@ export const WORKEXPERIENCES: WorkExperience[] = [
                         name: 'Manulife Plaza Building, 75 Hoang Van Thai, Tan Phu Ward, Dist. HCM City (on-site)'
                     }
                 ],
-                role: [{ title: 'Front-end' }],
+                role: [{ title: 'Front-End' }],
                 teamsize: 5,
                 task: [
                     { name: 'Standing daily meeting' },
@@ -52,6 +52,118 @@ export const WORKEXPERIENCES: WorkExperience[] = [
                     { name: 'Flex Layout' },
                     { name: 'Angular Material' },
                 ]
+            },
+            {
+                id: '2',
+                name: 'kinh nghiệm học hành ở pluralSight.com',
+                fromdate: 'OCTOBER, 2020',
+                todate: 'DECEMBER, 2020',
+                address: [
+                    { name: 'PluralSight.com' },
+                ],
+                role: [{ title: 'Front-End' }],
+                teamsize: 1,
+                task: [
+                    { name: 'Watching video and learning by doing' },
+                ],
+                achievement: [
+                    {
+                        name: 'Experience self-learning basic with Angular programing'
+                    },
+                ],
+                project: [
+                    { name: `Angular Basic, Angular Routing, Angular HTTP client module` }
+                ],
+                technology: [
+                    { name: 'TypeScript, Angular, Ng CLI, HTML, CSS, JS ' },
+                ],
+                tool: [
+                    { name: 'VS Code, Git Hub' },
+                ],
+            },
+            {
+                id: '3',
+                name: 'kinh nghiệm sml ở NETSPACE',
+                fromdate: 'JULY, 2020',
+                todate: 'AUGUST, 2020',
+                address: [
+                    { name: 'NETSPACE, DISTRICT 3, MARKETING DEPARTMENT, FULL-TIME' },
+                ],
+                role: [{ title: 'Front-End' }],
+                teamsize: 4,
+                task: [
+                    { name: 'daily meetings, maintaining code, daily report' },
+                ],
+                achievement: [
+                    {
+                        name: 'Experience programing with pure HTML projects. Experience programing with Data visualization. Experience edit image with Photoshop and AI'
+                    },
+                ],
+                project: [
+                    { name: `huongnghiep.com.vn, netspace.com.vn, Saigon Chef` }
+                ],
+                technology: [
+                    { name: 'Owl-carousel, HTML5, CSS3, JavaScript, jQuery, Bootstrap 4 Framework, CSS3 animation, Chart.js' },
+                ],
+                tool: [
+                    { name: 'Photoshop, AI, VS code, FileZilla' },
+                ],
+            },
+            {
+                id: '4',
+                name: 'kinh nghiệm sml ở WAHSIS',
+                fromdate: 'NOVEMBER, 2018',
+                todate: 'JULY, 2020',
+                address: [
+                    { name: 'WAHSIS CO, LTD DISTRICT 7, SOFTWARE DEPARTMENT, FULL-TIME' },
+                ],
+                role: [{ title: 'Front-End' }],
+                teamsize: 5,
+                task: [
+                    { name: 'Creating new, maintaining feature, meeting weekend' },
+                ],
+                achievement: [
+                    {
+                        name: 'Experience programing AngularJS, HTML/CSS/JS, Photoshop, AI'
+                    },
+                ],
+                project: [
+                    { name: `PMS and SMS Apartment management and contract management of high-rise apartments, townhouses system` }
+                ],
+                technology: [
+                    { name: 'AngularJS, Bootstrap 3, JavaScript, HTML, CSS, JS, jQuery,' },
+                ],
+                tool: [
+                    { name: 'VS Code, Photoshop, AI, Tortoise git, JIRA' },
+                ],
+            },
+            {
+                id: '4',
+                name: 'kinh nghiệm sml ở ISC Quang Trung',
+                fromdate: '2016',
+                todate: '2017',
+                address: [
+                    { name: 'INTERN STUDENT, ISC QUANG TRUNG, DISTRICT 12 HCMC, LAB 1, FULL-TIME' },
+                ],
+                role: [{ title: 'Front-End' }],
+                teamsize: 3,
+                task: [
+                    { name: 'Assisting member SQL design table, maintaining in project, daily meeting' },
+                ],
+                achievement: [
+                    {
+                        name: 'Experience programing with AngularJS, NodeJS'
+                    },
+                ],
+                project: [
+                    { name: `ISC Student management` }
+                ],
+                technology: [
+                    { name: 'AngularJS, Bootstrap 3, JavaScript, HTML, CSS, JS, jQuery, Photoshop' },
+                ],
+                tool: [
+                    { name: 'My SQL, Corel Draw, red mine, MS Project' },
+                ],
             }
         ]
     }
