6b75537ea34ef50c87d849aa2cd471d9c609a0cb
sokkimthanh1 <thanhsk1991@gmail.com>
2021-02-11 09:38:57 +0700

    update route guard

--- src/app/routing/can-activate-route.guard.ts
+++ src/app/routing/can-activate-route.guard.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRoute } from '@angular/router';
+import { CanActivate, ActivatedRoute, ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class CanActivateRouteGuard implements CanActivate {
-    id: string;
-    url: string;
-    user;
-    constructor(route: ActivatedRoute) {
-        this.id = route.snapshot.params.id;
-        this.url = route.snapshot.url.join('');
-        this.user = route.snapshot.data.user;
+    constructor(private router: Router) { }
 
-        route.url.subscribe(() => {
-            console.log(route.snapshot.params.id);
-        });
-    }
-    // tslint:disable-next-line:max-line-length
-    canActivate(): boolean {
-        if (this.url !== 'cv-sokkimthanh') {
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        const id = +next.url[1].path;
+        if (isNaN(id) || id < 1) {
+            alert('Invalid product Id');
+            this.router.navigate(['/cv-sokkimthanh']);
             return false;
-        } else {
-            return true;
         }
+        return true;
     }
 }
--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -4,7 +4,7 @@ import { PageNotFoundComponent } from '../page-not-found/page-not-found.componen
 import { CanActivateRouteGuard } from './can-activate-route.guard';
 
 export const ROUTES: Routes = [
-    { path: 'cv-sokkimthanh', component: CvComponent, canActivate: [CanActivateRouteGuard] },
+    { path: 'cv-sokkimthanh', component: CvComponent },
     { path: '', redirectTo: 'cv-sokkimthanh', pathMatch: 'full' }, // redirect to `first-component`
     { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ]; // sets up routes constant where you define your routes
